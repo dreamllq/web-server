705dc5efe170eb6e39c95b3e901193c1231202f0
lvliqi <lvliqi_d@163.com>
2024-04-19 11:35:42 +0800

    feat: up

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # server
 
+免费redis/mysql
+
+https://factory.methodot.com/
+
+dev环境启动前复制 .env.default => .env.test
+生产环境启动前复制 .env.default => .env
--- src/data-source.ts
+++ src/data-source.ts
@@ -2,8 +2,8 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: '39.101.77.81',
-  port: 7897,
+  host: '842bdf3688de.c.methodot.com',
+  port: 31483,
   username: 'root',
   password: '123456',
   database: 'test',
