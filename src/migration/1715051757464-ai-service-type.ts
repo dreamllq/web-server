10b7a7f74fbda251c57d6dcc80f99772846ff5f6
lvliqi <lvliqi_d@163.com>
2024-05-07 11:20:27 +0800

    feat: add init data

---
+++ src/migration/1715051757464-ai-service-type.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AiServiceType1715051757464 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('INSERT INTO `ai_service_type`(`id`,`name`) VALUES(\'49197683-2c76-4db5-aa39-28950c25d013\',\'文本对话\');');
+    await queryRunner.query('INSERT INTO `ai_service_type`(`id`,`name`) VALUES(\'906006d9-3e3a-41e8-bae4-889d7e1861c6\',\'文本生图\');');
+    await queryRunner.query('INSERT INTO `ai_service_type`(`id`,`name`) VALUES(\'2e0d9549-0ef3-44b1-a7fc-422aef5d9551\',\'图像理解\');');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+  }
+
+}
