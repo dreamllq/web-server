102283bd2827adfe3222795f443b03735a7e881b
lvliqi <lvliqi_d@163.com>
2025-01-26 11:11:41 +0800

    feat: up

--- src/bi/bi.module.ts
+++ src/bi/bi.module.ts
@@ -1,9 +1,4 @@
 import { Module } from '@nestjs/common';
-import { DataModule } from './data/data.module';
-import { ChartModule } from './chart/chart.module';
-import { ViewModule } from './view/view.module';
 
-@Module({
-  imports: [DataModule, ChartModule, ViewModule]
-})
+@Module({ imports: [] })
 export class BiModule {}
--- src/bi/chart/meta/dto/update-meta.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMetaDto } from './create-meta.dto';
-
-export class UpdateMetaDto extends PartialType(CreateMetaDto) {}
